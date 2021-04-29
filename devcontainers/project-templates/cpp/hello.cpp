2ef6ba0ffb9708ab2e5d709cb2ff5cb664dbdba1
kevamej <82548848+kevamej@users.noreply.github.com>
2021-04-28 20:51:56 -0400

    project-templates/cpp/hello.cpp

--- devcontainers/project-templates/cpp/hello.cpp
+++ devcontainers/project-templates/cpp/hello.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
-#include <vector>
 
-int
-main ()
+using namespace std;
+
+int main ()
 {
-  std::vector<int> v;
-  for (int i : {5, 4, 3, 2, 1})
-  {
-    v.push_back (i);
+  for (int i = 1; i <= 55; ++i) {
+    std::cout << i << " ";
   }
-
-  for (auto val : v)
-  {
-    std::cout << val << ' ';
-  }
-  std::cout << "\nHello, World!" << std::endl;
+  
+  
+  
+  std::cout << "\nHello, World! HAHAHA" << std::endl;
 
   return 0;
 }
